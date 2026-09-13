78b0dbca12bfd65535b1e470537d38cc01617269
daylily <i@dayli.ly>
2026-09-13 16:39:25 -0400

    tsdown: improve build process
    Enable the optional attw and publint checks, turn on unbundle, and set platform to `neutral`.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/2colons.ts'],
   dts: true,
+  unbundle: true,
+  platform: 'neutral',
+  publint: true,
+  attw: true,
 })
